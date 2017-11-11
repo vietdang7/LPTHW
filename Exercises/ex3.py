67aa16f5a88edb6a1c4d6b27e5719ec565d9cdf6
Viet Dang Quoc <thedangs7@gmail.com>
2017-11-11 21:20:11 +0200

    Update READEME.md with new exercise note for ex3.py

---
+++ Exercises/ex3.py
@@ -0,0 +1,33 @@
+# Print out a string
+print("I will now count my chickens:")
+
+# Print a string and result of a calculation
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Print another string
+print("Now I will count the eggs")
+
+# Print result of a calculation
+print(3 + 2 +1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Print a string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Print a comparation between 2 calculations, will be TRUE or FALSE
+print(3 + 2 < 5 - 7)
+
+# Print a string and result of a calculation
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# Print a string
+print("Oh, that's why it's FALSE")
+
+# Print a string
+print("How about some more.")
+
+# Print a string and boolean value
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)

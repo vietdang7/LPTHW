2125e0180d8ee1df831b0011b3ab95ca06bde494
Viet Dang Quoc <thedangs7@gmail.com>
2017-11-11 20:46:37 +0200

    Rename Exercise 1 folder to Exercises and create ex2.py

---
+++ Exercises/ex2.py
@@ -0,0 +1,8 @@
+# A comment to read my program later.
+# Anything after the # is ignored by Python
+print("I could have code like this.") # and the comment after this is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")

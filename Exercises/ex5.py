ba0e9312b8533271db7227fa2b7e09fbffc4cf79
Viet Dang Quoc <thedangs7@gmail.com>
2017-11-17 19:33:09 +0200

    Add ex5.py

---
+++ Exercises/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Viet Dang'
+my_age = 31
+my_height = 165 #cm
+my_weight = 80 #kg
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = "Black"
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cms tall.")
+print(f"He's {my_weight} kgs heavy.")
+print("Actually it's overweight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f" If I add {my_age}, {my_height}, {my_weight} I will get {total}. ")
